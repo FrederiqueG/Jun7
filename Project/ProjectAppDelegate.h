481be30d11d048da36ef2eb298155a664aaacecb
FrederiqueG <frederique.grd@gmail.com>
2012-06-07 13:54:21 -0400

    initial commit of Project

---
+++ Project/ProjectAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ProjectAppDelegate.h
+//  Project
+//
+//  Created by Frederique Giraud on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
